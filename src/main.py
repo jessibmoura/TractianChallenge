a8a3fdd984bae394c673c2afa74ff5c920f9f676
jessibmoura <jessicabmoura00@gmail.com>
2025-05-11 14:39:29 -0400

    testing pipeline for different product page

--- src/main.py
+++ src/main.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     baldor = Baldor()
 
     baldor.open()
-    baldor.load_products_page(target_category="Grinders, Buffers, Lathes")
+    baldor.load_products_page(target_category="DC Motors")
     baldor.scrap_product(limit=5)
     baldor.exit()
